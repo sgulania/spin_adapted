4aa1e85e10412ac0318909f65aa70e5ad48a78e0
James Daniel Whitfield <james.d.whitfield@dartmouth.edu>
2017-08-30 19:23:47 -0400

    Got the PrimDiffs implemented.
    Next to compute the matrix elements with the results from PrimDiffs

--- SAuYF/fci_cpp/minimal.cpp
+++ SAuYF/fci_cpp/minimal.cpp
@@ -11,7 +11,6 @@
 
 int
 PrimDiffs(int M, const std::vector<int>& primRef, const std::vector<int>& primK,
-          std::vector<int>&       virt  , std::vector<int>&        occ, 
 	  std::vector<int>&         Pr  , std::vector<int>&         Pk ) ;
 
 int 
@@ -26,13 +25,60 @@ main()
 
 	vector<int> occ;
 	vector<int> virt;
-	vector<int> left  {1,5,3,7,2,2,6};
-	vector<int> right {1,9,5,4,7,3,6};
-
 	vector<int> Pr;
 	vector<int> Pk;
-	PrimDiffs(10,left,right,occ,virt,Pr,Pk);
 
+	//unit tests
+	{
+	vector<int> left  {1,5,3,7,2,2,6};
+	vector<int> right {1,9,5,4,7,3,6};
+	auto ndiff=PrimDiffs(10,left,right,Pr,Pk);
+	std::cout<< "diffs: "<<ndiff;
+        }
+	/*
+EXPECTED DEBUGGING OUTPUT
+
+empty:0 8 
+filled:1 3 5 6 7 
+virt:4 9 
+occ:2 2 
+sorting permutation for ref: 5 4 0 2 1 6 3 
+input ref: 1 5 3 7 2 2 6 
+sorted ref: 2 2 1 3 5 6 7 
+
+sorting permutation for K: 3 1 0 5 2 6 4 
+input K: 1 9 5 4 7 3 6 
+sorted K: 4 9 1 3 5 6 7 
+	 */
+	
+        {
+	vector<int> left  {1,9,3,7,2,2,6};
+	vector<int> right {1,2,3,6,9,5,4};
+	auto ndiff=PrimDiffs(10,left,right,Pr,Pk);
+	std::cout<< "diffs: "<<ndiff;
+	}
+       /*
+EXPECTED DEBUGGING OUTPUT
+
+empty:0 8 
+filled:1 3 6 9 
+virt:4 5 
+occ:2 7 
+sorting permutation for ref: 5 4 3 0 2 6 1 
+input ref: 1 9 3 7 2 2 6 
+sorted ref: 2 2 7 1 3 6 9 
+
+sorting permutation for K: 6 5 0 1 2 3 4 
+input K: 1 2 3 6 9 5 4 
+sorted K: 4 5 1 2 3 6 9 
+*/
+
+	{
+	vector<int> left  {1,9,3,7,2,2,6};
+	vector<int> right {0,2,3,6,9,5,4};
+	PrimDiffs(10,left,right,Pr,Pk);
+	}
+	
 	vector<int> Peye {0,1,2};
 	vector<int> P12  {1,0,2};
 	vector<int> P23  {0,2,1};
@@ -71,16 +117,21 @@ perm_multiply(const std::vector<int>& P1, const std::vector<int>& P2,
 	return 0;
 }
 
+/*
+PRIMDIFFS takes 
+   1. the total number of orbitals (or the maximally occupied index)
+   2. the primative reference 
+   3. the primative K
+   4. at return Pr has the permuation to put things in order
+          
+ */ 
 int // return excitation level
 PrimDiffs(int M, const std::vector<int>& primRef, const std::vector<int>& primK,
-          std::vector<int>&       virt  , std::vector<int>&        occ, 
 	  std::vector<int>&         Pr  , std::vector<int>&         Pk) 
 {
 
 
     //empty containers
-    virt.clear();
-    occ.clear();
     Pk.clear();
     Pr.clear();
 
@@ -105,6 +156,10 @@ PrimDiffs(int M, const std::vector<int>& primRef, const std::vector<int>& primK,
 	    unsortedK[primK[j]]++;
 
 
+    //these are the orbitals that are occupied in ref but not in K
+    std::vector<int> occ;
+    //these are the orbitals that are occupied in K but not in ref
+    std::vector<int> virt;
 
     //these are the orbitals that are empty in both
     std::vector<int> empty;
@@ -149,6 +204,7 @@ PrimDiffs(int M, const std::vector<int>& primRef, const std::vector<int>& primK,
 
     if(debug)
     {
+	    std::cout << "\n";
 	    std::cout << "empty:";
             for(auto i : empty) std::cout << i << " ";
 	    std::cout << "\n";
@@ -209,173 +265,42 @@ PrimDiffs(int M, const std::vector<int>& primRef, const std::vector<int>& primK,
     //DEBUGGING/TESTING
     if(debug)
     {
-	for (int i = 0 ; i != Pr.size() ; i++) 
-	{
-	   std::cout << Pr[i] << std::endl;
-	}
+	std::cout << "sorting permutation for ref: ";
+	for (auto s : Pr)
+		std::cout << s << " ";
+	std::cout << std::endl;
 
 	std::cout << "input ref: " ;
 	for(int i=0; i!=primRef.size(); i++)
 	   std::cout << primRef[i] << " ";
 	std::cout << std::endl;
+
 	std::cout << "sorted ref: ";
 	for(int i=0; i!=Pr.size(); i++)
 	std::cout << primRef[Pr[i]] << " ";
+	std::cout << std::endl;
 
 	std::cout << std::endl;
 
-        for (int i = 0 ; i != Pk.size() ; i++) 
-	   std::cout << Pk[i] << std::endl;
+	std::cout << "sorting permutation for K: ";
+	for (auto s : Pk)
+		std::cout << s << " ";
+	std::cout << std::endl;
 
 	std::cout << "input K: " ;
 	for(int i=0; i!=primK.size(); i++)
 	   std::cout << primK[i] << " ";
 	std::cout << std::endl;
+
 	std::cout << "sorted K: ";
 	for(int i=0; i!=Pk.size(); i++)
 		std::cout << primK[Pk[i]] << " ";
-
 	std::cout << std::endl;
 
     }
 
     return occ.size(); // this is the excitation level
 
-    // new logic
-    //   virtuals
-    //   occ
-    //   loop over occ orbs in ref
-    //      look for a partner in primK, pop it off the list of virtuals
-
-
-    // %%%%%%%% find first orb in reference not in K %%%%%%%%
-    for(int occ_orb_idx=0; occ_orb_idx<N; occ_orb_idx++) //loop through occ ref orbs
-    {
-        //pull occupied orbital label
-        auto occ_orb=primRef[Pr[occ_orb_idx]];
-
-        //flag to say if it was found
-        found=false;
-
-        for(int K_orb_idx=0; K_orb_idx<N; K_orb_idx++) //see if its in the K prim
-        {
-            if(occ_orb==primK[Pk[K_orb_idx]]) // found orb from ref in prim K
-            {
-                found=true;
-                break;
-            }
-        }
-
-        if(!found) // found orbital in ref not in K
-        {
-            //increase excitation level
-            n_ex_lvl++;
-
-	   //include permutation of occ orbital to front in Pr (same info as orb_index marking it)
-
-
-
-	    std::vector<int> v(occ_orb_idx) ; 
-	    v[0]=occ_orb_idx-1;
-	    for(int j=1; j<occ_orb_idx; j++)
-	    {
-		    v[j]=j-1;
-	    }
-
-
-	    std::cout << "cyclic permutation\n";
-
-	    for(int j=0; j<occ_orb_idx; j++)
-		    std::cout << v[j] << " ";
-	    std::cout << std::endl;
-
-           /*******************************************************************/
-
-            //L_occ1=left_occupancy=orb_index
-            left_occupancy=occ_orb_idx;
-            
-            //% keep looking for second orb in ref not in K
-            //%   L_occ2=left_occupancy=orb_index - 1 // extra sub for occ1
-            if(n_ex_lvl==2)
-                left_occupancy=occ_orb_idx-1;
-           /*******************************************************************/
-
-
-
-            //% keep looking for third orb in ref not in K
-            //%   if you find it, stop and return as H_JK=0
-            if(n_ex_lvl==3)
-                return 3;
-            
-            //save orb 
-            occ.push_back(occ_orb);
-
-        }
-    }	
-
-       //loop through occ ref orbs O(N) cost
-         //check against orbs of K O(N)
-	 //if not found
-	   //increase excitation level
-	   
-    // if all the same return P_r, P_k
-    //no need to go through K if all are orbs the same
-    if(n_ex_lvl==0)
-        return 0;
-
-
-    // %%%%%%%% find orb in K not in reference %%%%%%%%
-    for(int K_orb_idx=0; K_orb_idx<N; K_orb_idx++)
-    {
-        //pull orbital number
-        auto K_orb=primK[Pk[K_orb_idx]];
-
-        found=false;
-        for(int occ_orb_idx=0; occ_orb_idx<N; occ_orb_idx++)
-        {
-            if(K_orb==primRef[Pr[occ_orb_idx]])
-            {
-                found=true;
-                break;
-            }
-        }
-
-        if(!found)
-        {
-
-            //save data
-            virt.push_back(K_orb);
-            
-            //taking into account previously found
-            left_occupancy=K_orb_idx - (virt.size()-1);
-
-            //accumulate phase
-            //if(left_occupancy%2)
-            //    phase*=-1;
-
-            //stop looking if we only need one
-            if(n_ex_lvl==1)
-                return n_ex_lvl;
-            
-            //stop looking if we found two
-            if(virt.size()==2)
-                return n_ex_lvl;
-
-        }
-
-    }	
-    
- 
-    printf("\n\nSomething went wrong in PrimDiffs, logic broken\n");
-    throw;
-    return -9;
-
-
-       //loop through occ K orbs O(N) cost
-         //check against orbs of ref, O(N)
-	 //if not found
-	    //include permutation of virt orb to front in Pk
-            //do we need to find another? if yes, continue else return: Pr, Pk, virt_indicies, occ_indicies
 }
 
 
