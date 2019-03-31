dd07a586024e97d28d015735ac13cd9260f1f40e
Sahil Gulania <gulania@usc.edu>
2019-03-31 15:26:54 -0700

    comment_hamiltonian

--- 3_electron_half_spin/hamiltonian.f90
+++ 3_electron_half_spin/hamiltonian.f90
@@ -1,25 +1,39 @@
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-!Subroutine for evaluating Hamiltonian matrix
-!Hardcoded for N=3, S=1/2
-!Input - Wavefunction 
-!Output - Spectrum
+! Subroutine for evaluating Hamiltonian matrix
+! Hardcoded for N=3, S=1/2 but can be easily extened
+! fof general case
+! Input - Wavefunction 
+! Output - Spectrum
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-subroutine hamiltonian(cof_p11,cof_p21,p11,p21,list_p11,list_p21,n_p11,n_p21)
+subroutine hamiltonian(cof_phi_1,cof_phi_2,phi_1,phi_2,list_phi_1,list_phi_2,n_phi_1,n_phi_2)
 USE variables
 implicit none
 
-integer i,j,k,l,m,n,o
-integer n_p11,n_p21
-integer list_p11(n_weyl+1),list_p21(n_weyl+1),LWORK_f,LWORK_S,INFO
-real*8 cof_p11(n_p11),cof_p21(n_p21)
-integer p11(n_p11,n_el), p21(n_p21,n_el)
+
+integer i,j
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! Wavefunction information
+
+integer n_phi_1,n_phi_2
+integer list_phi_1(n_weyl+1),list_phi_2(n_weyl+1)
+integer phi_1(n_phi_1,n_el), phi_2(n_phi_2,n_el)
+real*8 cof_phi_1(n_phi_1),cof_phi_2(n_phi_2)
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! Variable for orthogonalization and digonalization
+integer LWORK_f,LWORK_S,INFO
 real*8 H_f(n_weyl,n_weyl), S_f(n_weyl,n_weyl), W_f(n_weyl),WORK_f(3*n_weyl-1)
 real*8 Ov(n_weyl,n_weyl), W_S(n_weyl),WORK_S(3*n_weyl-1) 
 real*8 X_S(n_weyl,n_weyl), H_S(n_weyl,n_weyl)
 real*8 X_symm(n_weyl,n_weyl)
-integer i11,i11n,j11,j11n,i21,i21n,j21,j21n
-real*8 val,over
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+integer i11,i11n,j11,j11n,i21,i21n,j21,j21n  !dummy variables
+real*8 val,over                              !storing hamiltonian value and overlap
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 ! Using the list of phi_1 and phi_2 to evaluate 
@@ -29,24 +43,23 @@ real*8 val,over
 do i=1,n_weyl
     do j=1,n_weyl
 
- !      write(6,*) i,j
-       i11  = list_p11(i)+1
-       i11n = list_p11(i+1)
+       i11  = list_phi_1(i)+1
+       i11n = list_phi_1(i+1)
 
-       j11  = list_p11(j)+1
-       j11n = list_p11(j+1)
+       j11  = list_phi_1(j)+1
+       j11n = list_phi_1(j+1)
       
-       i21  = list_p21(i)+1
-       i21n = list_p21(i+1)
+       i21  = list_phi_2(i)+1
+       i21n = list_phi_2(i+1)
        
-       j21  = list_p21(j)+1
-       j21n = list_p21(j+1)
+       j21  = list_phi_2(j)+1
+       j21n = list_phi_2(j+1)
        
        
-       call one_element(p11(i11:i11n,:),p21(i21:i21n,:),&
-                        p11(j11:j11n,:),p21(j21:j21n,:),&
-                    cof_p11(i11:i11n),cof_p21(i21:i21n),&
-                    cof_p11(j11:j11n),cof_p21(j21:j21n),&
+       call one_element(phi_1(i11:i11n,:),phi_2(i21:i21n,:),&
+                        phi_1(j11:j11n,:),phi_2(j21:j21n,:),&
+                    cof_phi_1(i11:i11n),cof_phi_2(i21:i21n),&
+                    cof_phi_1(j11:j11n),cof_phi_2(j21:j21n),&
                     i11n-i11+1,i21n-i21+1,j11n-j11+1,j21n-j21+1,val,over)
 
       S_f(i,j)=over
